fabe032fee4f703954d6d2219d19751a003d3685
vargattj <vargattjh@gmail.com>
2020-09-25 11:45:29 -0300

    aplicando unform

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,40 +1,52 @@
 import React from 'react';
-
 import { FiLogIn, FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+import { Form } from '@unform/web';
+
 import logo from '../../assets/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
 import { Container, Content, Background } from './styles';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
 
-    <Content>
-      <img src={logo} alt="" />
+  return (
+    <Container>
+      <Background />
 
-      <form>
-        <h1>Faça seu Cadastro</h1>
-        <Input name="name " icon={FiUser} type="text" placeholder="Nome" />
+      <Content>
+        <img src={logo} alt="" />
 
-        <Input name="email " icon={FiMail} type="email" placeholder="E-mail" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Senha"
-        />
+        <Form onSubmit={handleSubmit} initialData={{ name: 'Johnny' }}>
+          <h1>Faça seu Cadastro</h1>
+          <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
 
-        <Button>Cadastrar</Button>
-      </form>
+          <Input
+            name="email "
+            icon={FiMail}
+            type="email"
+            placeholder="E-mail"
+          />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
 
-      <a href="login">
-        <FiArrowLeft />
-        Voltar pra Logon
-      </a>
-    </Content>
-  </Container>
-);
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="login">
+          <FiArrowLeft />
+          Voltar pra Logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
