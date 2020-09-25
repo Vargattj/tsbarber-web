0492174ac35429f47a9ae16da6b185f3e7eeef7a
vargattj <vargattjh@gmail.com>
2020-09-25 08:28:26 -0300

    estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
-const App: React.FC = () => <h1>a</h1>;
+const App: React.FC = () => (
+  <>
+    <h1>a</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
